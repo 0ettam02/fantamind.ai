02f463f5579f7fc7c690a7a03de236b21b956fe7
aruta <matteoaruta6@gmail.com>
2024-10-24 20:56:20 +0200

    updated other dependences to the last version

--- src/app/components/heroComponent.js
+++ src/app/components/heroComponent.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { useEffect } from "react";
 import { Ruda } from "next/font/google";
 
@@ -8,12 +8,11 @@ const ruda = Ruda({
 });
 
 export default function Hero() {
-
   return (
     <>
       <div className="hero flex justify-center items-center text-center mt-10">
         <h1 className={`${ruda.className} text-5xl md:text-8xl`}>
-          Dove <span className="bg-[#00FF26]">l'intuizione</span>
+          Dove <span className="bg-[#00FF26]">l&apos;intuizione</span>
           <br />si fonde con la
           <br />
           <span className="bg-[#00FF26]">logica</span>
